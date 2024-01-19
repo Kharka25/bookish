aa206b62c608250851c3dee58cc5e9caec1945c4
kazeem <kazeem.kayode@newsquest.co.uk>
2024-01-19 18:27:50 +0000

    Android build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Alert} from 'react-native';
 import {hasCrashedInLastSession} from 'appcenter-crashes';
 
 import {AuthNavigator} from '@navigation';
 
 const App: React.FC = () => {
-  async function checkPreviousSession() {
+  (async function checkPreviousSession() {
     const didCrash = await hasCrashedInLastSession();
-
+    console.log(didCrash);
     if (didCrash) {
-      console.log(didCrash);
       Alert.alert("Sorry about that crash, we're working on a solution");
     }
-  }
+  })();
 
-  useEffect(() => {
-    checkPreviousSession();
-  }, []);
+  // checkPreviousSession();
+
   return <AuthNavigator />;
 };
 export default App;
